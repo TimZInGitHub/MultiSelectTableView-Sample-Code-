2849795865867a99ca584243c580955b64e5c34e
TimZInGitHub <Tim@11234.local>
2015-11-24 02:12:20 +0800

    Initial Commit

---
+++ MultiSelectTableView(Sample Code)Tests/MultiSelectTableView_Sample_Code_Tests.m
@@ -0,0 +1,39 @@
+//
+//  MultiSelectTableView_Sample_Code_Tests.m
+//  MultiSelectTableView(Sample Code)Tests
+//
+//  Created by Tim.Z on 11/24/15.
+//  Copyright © 2015 Tim.Z. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MultiSelectTableView_Sample_Code_Tests : XCTestCase
+
+@end
+
+@implementation MultiSelectTableView_Sample_Code_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
